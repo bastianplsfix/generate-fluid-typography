67dc9a0e4cbabad7ee3627e20ce0f6445a39b468
bastianplsfix <hello@bastian.sh>
2023-12-12 12:48:35 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,29 @@ import './App.css'
 function App() {
   return (
     <>
-    <div className="display">Display text here</div>
-    <div className="heading-1">Title goes here</div>
-    <div className="heading-2">Sub-title goes here</div>
-    <p className="lead">Leading paragraph text here</p>
-    <p className="body">Body text goes here</p>
-    <p className="small">Small font size text goes here</p>
-    <p className="label">Label text goes here</p>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <h1 className="display">My First Blog Post</h1>
-    <p className="body">Date: December 8, 2023</p>
-    <p className="body">By: John Doe</p>
-    <h2 className="subtitle">Introduction</h2>
-    <p className="body">This is the opening paragraph of my blog post, where I set the stage for the reader and summarize what I'll be discussing.</p>
-    <h2 className="subtitle">Body</h2>
-    <p className="body">This is the main part of my blog post, where I go into detail on the topic I introduced in the opening paragraph.</p>
-    <h2 className="subtitle">Conclusion</h2>
-    <p className="body">This is the closing paragraph of my blog post, where I sum up everything I discussed and provide some final thoughts.</p>
+      <div className="display">Display text here</div>
+      <div className="heading-1">Heading-1 goes here</div>
+      <div className="heading-2">Heading-2 goes here</div>
+      <div className="heading-3">Heading-3 goes here</div>
+      <p className="lead">Leading paragraph text here</p>
+      <p className="body">Body text goes here</p>
+      <p className="small">Small font size text goes here</p>
+      <p className="label">Label text goes here</p>x
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <h1 className="display">My First Blog Post</h1>
+      <p className="body">Date: December 8, 2023</p>
+      <p className="body">By: John Doe</p>
+      <h2 className="subtitle">Introduction</h2>
+      <p className="body">This is the opening paragraph of my blog post, where I set the stage for the reader and summarize what I'll be discussing.</p>
+      <h2 className="subtitle">Body</h2>
+      <p className="body">This is the main part of my blog post, where I go into detail on the topic I introduced in the opening paragraph.</p>
+      <h2 className="subtitle">Conclusion</h2>
+      <p className="body">This is the closing paragraph of my blog post, where I sum up everything I discussed and provide some final thoughts.</p>
     </>
   )
 }
